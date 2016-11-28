420825714db78f5560819e9384c242af45794528
Happy Mittal <happy2332@gmail.com>
2016-11-28 16:54:04 +0100

    after food code

--- experiments/graphclassification/spngraphlet.py
+++ experiments/graphclassification/spngraphlet.py
@@ -1,44 +1,154 @@
 
 from collections import OrderedDict
+from cvxpy import *
 import glob
+from joblib.memory import Memory
+from joblib.test.test_pool import check_array
 import json
+import numpy
 import os
 from pprint import pprint
-from statistics import mean, stdev
-
-from joblib.memory import Memory
-import numpy
 from sklearn import preprocessing
 from sklearn import svm, cross_validation
+from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.cross_validation import train_test_split, StratifiedKFold, KFold
 from sklearn.grid_search import GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, metrics
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics.ranking import roc_curve, auc
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn.utils.multiclass import unique_labels
+from sklearn.utils.validation import check_X_y, check_is_fitted
+from statistics import mean, stdev
 from statsmodels.genmod.cov_struct import Independence
+import time
 
 from algo.learnspn import LearnSPN
 from pdn.independenceptest import getPval
 
-import time
 
-memory = Memory(cachedir="/data/d1/molina/spn", verbose=0, compress=9)
+memory = Memory(cachedir="/tmp/spn3", verbose=0, compress=9)
 
 path = os.path.dirname(__file__)
 
+
+class SPNClassifier(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, alpha=0.001, min_instances_slice=80, families="poisson"):
+        self.alpha = alpha
+        self.min_instances_slice = min_instances_slice
+        self.families = families
+
+
+    def get_params(self, deep=True):
+        # suppose this estimator has parameters "alpha" and "recursive"
+        return {"alpha": self.alpha, "min_instances_slice": self.min_instances_slice, "families": self.families}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            self.setattr(parameter, value)
+        return self
+
+    def fit(self, X, y):
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+        # Store the classes seen during fit
+        self.classes_ = unique_labels(y)
+        print(y.shape, numpy.unique(y))
+        print(self.classes_)
+        
+        #0/0
+
+        self.X_ = X
+        self.y_ = y
+        # Return the classifier
+        
+        
+        # classes = numpy.unique(Y)
+        self.spns_ = []
+        
+        self.ws_ = []
+        trainll = numpy.zeros((X.shape[0],self.classes_.shape[0]))
+        for j in range(self.classes_.shape[0]):
+            idx = y == self.classes_[j]
+            #self.ws_.append(float(numpy.sum(idx)) / X.shape[0])
+            
+            data_train_class = X[idx, :]
+            spn = LearnSPN(alpha=self.alpha, min_instances_slice=self.min_instances_slice, cluster_prep_method="sqrt", families=self.families, cache=memory).fit_structure(data_train_class)
+            self.spns_.append(spn)
+            trainll[idx, j] = spn.eval(data_train_class, individual=True)
+        
+        
+        #self.ws_ = self.ws_/numpy.sum(self.ws_)
+        
+        
+        x = Variable(self.classes_.shape[0])
     
-def evalModel(predictor, train_data, train_labels, test_data, test_labels, name, evalresults):
+        constraints = [sum_entries(x) == 1, x > 0]
+        
+        A = numpy.exp(trainll)
+            
+        objective = Maximize(sum_entries(log(A * x)))
+        prob = Problem(objective, constraints)
+        prob.solve()
+        
+        
+        self.ws_ = sum(x.value.tolist(), [])
+        #print("Optimal w",self.ws_)
+        
+        return self
+
+    def predict(self, X):
+
+        # Check is fit had been called
+        check_is_fitted(self, ['X_', 'y_'])
+
+        # Input validation
+        #X = check_array(X)
+        
+        print(X.shape)
+        
+        if not isinstance(X, numpy.ndarray):
+            raise RuntimeError("input not an array")
+        
+        if X.shape[1] != self.X_.shape[1]:
+            raise RuntimeError("invalid number of dimensions")
+        
+        #if numpy.any(X < 0):
+        #    raise RuntimeError("invalid domain")
+                
+        if numpy.any(numpy.isnan(X)):
+            raise RuntimeError("invalid type")
+        
+
+        trainll = numpy.zeros((X.shape[0], self.classes_.shape[0]))
+    
+        for j, spn in range(self.classes_.shape[0]):
+            trainll[:, j] = spn.eval(X, individual=True) + numpy.log(self.ws_[j])
+    
+        #return self.clflr_.predict(trainll)
+    
+        pmax = numpy.argmax(trainll, axis=1)
+    
+        return self.classes_[pmax], trainll
+
+    
+#check_estimator(SPNClassifier)
+    
+def evalModel(predictor, test_data, test_labels, train_data, train_labels, name, evalresults):
 
     
     predictor.fit(train_data, train_labels)
-    evalresults.setdefault(name + " Accuracy raw \t\t", []).append(metrics.accuracy_score(test_labels, predictor.predict(test_data)))
+    evalresults.setdefault(name + " Accuracy raw \t\t", []).append(accuracy_score(test_labels, predictor.predict(test_data)))
 
-    predictor.fit(preprocessing.scale(train_data), train_labels)
-    evalresults.setdefault(name + " Accuracy std \t\t", []).append(metrics.accuracy_score(test_labels, predictor.predict(preprocessing.scale(test_data))))
-    
-    predictor.fit(preprocessing.normalize(train_data, norm='l2'), train_labels)
-    evalresults.setdefault(name + " Accuracy nml \t\t", []).append(metrics.accuracy_score(test_labels, predictor.predict(preprocessing.normalize(test_data, norm='l2'))))
+    #predictor.fit(preprocessing.scale(train_data), train_labels)
+    #evalresults.setdefault(name + " Accuracy std \t\t", []).append(metrics.accuracy_score(test_labels, predictor.predict(preprocessing.scale(test_data))))
+ 
+    #predictor.fit(preprocessing.normalize(train_data, norm='l2'), train_labels)
+    #evalresults.setdefault(name + " Accuracy nml \t\t", []).append(metrics.accuracy_score(test_labels, predictor.predict(preprocessing.normalize(test_data, norm='l2'))))
+
+    return
 
     if len(set(train_labels)) != 2:
         return
@@ -46,6 +156,8 @@ def evalModel(predictor, train_data, train_labels, test_data, test_labels, name,
     predictor.fit(train_data, train_labels)
     fpr, tpr, _ = roc_curve(test_labels, predictor.decision_function(test_data))
     evalresults.setdefault(name + " AUC raw \t\t", []).append(auc(fpr, tpr))
+
+    #return
 
     predictor.fit(preprocessing.scale(train_data), train_labels)
     fpr, tpr, _ = roc_curve(test_labels, predictor.decision_function(preprocessing.scale(test_data)))
@@ -58,12 +170,12 @@ def evalModel(predictor, train_data, train_labels, test_data, test_labels, name,
 
 
 
-#def evalspnCVFold(labels, data, train_index, test_index):
+# def evalspnCVFold(labels, data, train_index, test_index):
 
 
-def evalspnComplete(labels, data, dsname, writer, alpha, min_slices=50):
+def evalspnComplete(labels, data, dsname, writer, alpha, min_instances_slice=50):
     
-    cvfolds = StratifiedKFold(labels, n_folds=5, random_state=123)
+    cvfolds = StratifiedKFold(labels, n_folds=10, random_state=123)
     classes = list(set(labels))
     
     evalresults = OrderedDict()
@@ -75,12 +187,23 @@ def evalspnComplete(labels, data, dsname, writer, alpha, min_slices=50):
         test_data = data[test_index, ]
         test_labels = labels[test_index]
         
-        clfsvc = GridSearchCV(estimator=svm.SVC(kernel='linear', probability=True), param_grid=dict(C=numpy.logspace(-10, 0, 10)), n_jobs=50, cv=5)
-        clflr = LogisticRegression(solver='lbfgs')
-        
+        # clfsvc = GridSearchCV(estimator=svm.SVC(kernel='linear', probability=True), param_grid=dict(C=numpy.logspace(-10, 0, 10)), n_jobs=50, cv=5)
+        clfsvc = GridSearchCV(estimator=svm.SVC(kernel='linear', probability=True), param_grid={'C': [10 ** 3, 10 ** 2, 10 ** 1, 10 ** 0, 10 ** -1, 10 ** -2, 10 ** -3]}, n_jobs=50, cv=5)
         start = time.time()
         evalModel(clfsvc, test_data, test_labels, train_data, train_labels, "SVM raw", evalresults)
         evalresults.setdefault("SVM time in secs \t\t", []).append((time.time() - start))
+        
+        clspn = SPNClassifier(alpha=alpha, min_instances_slice=min_instances_slice)
+        start = time.time()
+        evalModel(clspn, test_data, test_labels, train_data, train_labels, "SPN NB raw", evalresults)
+        evalresults.setdefault("SPN time in secs \t\t", []).append((time.time() - start))
+        
+        #print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        #clflr = LogisticRegression(solver='lbfgs')
+        #start = time.time()
+        #evalModel(clflr, test_data, test_labels, train_data, train_labels, "LR NB raw", evalresults)
+        #evalresults.setdefault("SPN time in secs \t\t", []).append((time.time() - start))
+        continue
         
         evals_train = numpy.zeros((train_data.shape[0], 0))
         evals_test = numpy.zeros((test_data.shape[0], 0))
@@ -91,18 +214,24 @@ def evalspnComplete(labels, data, dsname, writer, alpha, min_slices=50):
         activations_train = numpy.zeros((train_data.shape[0], 0))
         activations_test = numpy.zeros((test_data.shape[0], 0))
         
+        
+        #model = ClassificationNBFit(train_data, train_labels)
+        
         timespn = 0
         for c in classes:
+            #break
             idx = train_labels == c
+            print(idx)
             data_train_class = train_data[idx, :]
             
             start = time.time()
-            spn = LearnSPN(alpha=alpha, min_instances_slice=min_slices, cluster_prep_method="sqrt", cache=memory).fit_structure(data_train_class)
-            #spn = spnlearn(data_train_class, alpha, min_slices=min_slices, cluster_prep_method="sqrt", family="poisson")
+            spn = LearnSPN(alpha=alpha, min_instances_slice=min_instances_slice, cluster_prep_method="sqrt", cache=memory).fit_structure(data_train_class)
+            print(alpha, min_instances_slice)
+            # spn = spnlearn(data_train_class, alpha, min_slices=min_slices, cluster_prep_method="sqrt", family="poisson")
             timespn += (time.time() - start)
             
-            #continue
-            evalperclass = numpy.asarray(spn.eval(train_data, individual=True)).reshape((train_data.shape[0],1))
+            # continue
+            evalperclass = numpy.asarray(spn.eval(train_data, individual=True)).reshape((train_data.shape[0], 1))
             print(evalperclass.shape)
             print(evalperclass)
             gradsperclass = spn.gradients(train_data)
@@ -113,18 +242,20 @@ def evalspnComplete(labels, data, dsname, writer, alpha, min_slices=50):
             grads_train = numpy.hstack((grads_train, gradsperclass))
             activations_train = numpy.hstack((activations_train, activationperclass))
             
-            evals_test = numpy.hstack((evals_test, numpy.asarray(spn.eval(test_data, individual=True)).reshape((test_data.shape[0],1))))
+            evals_test = numpy.hstack((evals_test, numpy.asarray(spn.eval(test_data, individual=True)).reshape((test_data.shape[0], 1))))
             grads_test = numpy.hstack((grads_test, spn.gradients(test_data)))
             activations_test = numpy.hstack((activations_test, spn.activations(test_data)))
             print("loop done")
             
         evalresults.setdefault("SPN time in secs \t\t", []).append(timespn)
          
+        
+        
         evalModel(clflr, evals_test, test_labels, evals_train, train_labels, "SPN per class ll -> LR", evalresults)
     
-        evalModel(clfsvc, grads_test, test_labels, grads_train, train_labels, "SPN per class gradients -> SVM", evalresults)
+        #evalModel(clfsvc, grads_test, test_labels, grads_train, train_labels, "SPN per class gradients -> SVM", evalresults)
         
-        evalModel(clfsvc, activations_test, test_labels, activations_train, train_labels, "SPN per class activations -> SVM", evalresults)
+        #evalModel(clfsvc, activations_test, test_labels, activations_train, train_labels, "SPN per class activations -> SVM", evalresults)
     
     
     writer.write(json.dumps(evalresults))
@@ -141,20 +272,21 @@ def run(classfname, datafname):
     classlabels = numpy.loadtxt(classfname)
     
     data = numpy.loadtxt(datafname, dtype=int, delimiter=",")
-    alpha=0.001
+    
+    alpha = 0.001
     print(data.shape)
     print(classlabels.shape)
     
-    with open("output1.txt", "a", buffering=2) as myfile:
+    with open("output5.txt", "a", buffering=2) as myfile:
         myfile.write(classfname + "\n")
         myfile.write(datafname + "\n")
-        myfile.write('%.30g'%(alpha) + "\n")        
-        evalspnComplete(classlabels, data, os.path.basename(datafname), myfile, alpha, min_slices=80)
+        myfile.write('%.30g' % (alpha) + "\n")        
+        evalspnComplete(classlabels, data, os.path.basename(datafname), myfile, alpha, min_instances_slice=80)
     
 if __name__ == '__main__':
     
     
-    for fname in sorted(glob.glob('wl/1*corpus.classes.csv'), reverse=True):
+    for fname in sorted(glob.glob('wl/2*corpus.classes.csv'), reverse=True):
         classfname = fname
         datafname = fname.replace(".classes", "")
         print("running for %s " % (datafname))
---
+++ experiments/softmax/deeperfood.py
@@ -0,0 +1,209 @@
+'''
+Created on Nov 17, 2016
+
+@author: molina
+'''
+import time
+
+from cvxpy import *
+from joblib.memory import Memory
+import numpy
+from sklearn.metrics.classification import accuracy_score
+from sklearn.metrics.ranking import roc_curve, auc
+
+from algo.learnspn import LearnSPN
+from experiments.graphclassification.spngraphlet import SPNClassifier
+import matplotlib.pyplot as plt
+
+
+numpy.set_printoptions(threshold=numpy.inf)
+
+memory = Memory(cachedir="/tmp", verbose=0, compress=9)
+
+def getTopIndices(arr):
+    # sort logits descanding
+    # There is no direct method of getting argsort descending, so first 
+    # sort in increasing, then reverse the list 
+    np_sorted_logits_asc = numpy.argsort(arr,1)
+    np_sorted_logits_desc = []
+    for _, e in enumerate(np_sorted_logits_asc):
+        np_sorted_logits_desc.append(e[::-1])
+    return np_sorted_logits_desc
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    # print(predicted)
+    # print(actual)
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    # print(score)
+    # if not actual:
+    #    return 0.0
+
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return numpy.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+def getOneHotEncoding(data, vocab_size=100):
+    result = numpy.zeros((data.shape[0], vocab_size))
+    
+    result[list(range(data.shape[0])), data] = 1
+    return result
+
+
+def spnClassificationNBPred(model, X):
+    
+    trainll = numpy.zeros((X.shape[0], model['classes'].shape[0]))
+    
+    for j, spn in enumerate(model["spns"]):
+        trainll[:, j] = spn.eval(X, individual=True) + numpy.log(model['weights'][j])
+
+    pmax = numpy.argmax(trainll, axis=1)
+
+    return (model['classes'][pmax], trainll)
+
+
+
+def spnClassificationNBFit(X, Y, alpha=0.001, min_slices=80):
+    classes = numpy.unique(Y)
+    spns = []
+    
+    # trainll = numpy.zeros((X.shape[0],classes.shape[0]))
+    ws = []
+    for j in range(classes.shape[0]):
+        idx = Y == classes[j]
+        ws.append(float(numpy.sum(idx)) / X.shape[0])
+        
+        data_train_class = X[idx, :]
+        spn = LearnSPN(cache=memory, alpha=alpha, min_instances_slice=min_slices, cluster_prep_method=None, families="gaussian").fit_structure(data_train_class)
+        spns.append(spn)
+        
+        # trainll[idx, j] = spn.eval(data_train_class, individual=True)
+        
+
+    return {'classes':classes, 'spns':spns, 'weights':ws}
+
+def spnClassificationGeneralFit(X, Y, maxClasses, alpha=0.001, min_slices=500):
+    # need to convert Y into one-hot encoding as there is no multinomial till now
+    #Y = getOneHotEncoding(Y, maxClasses)
+    print('X shape : ',X.shape)
+    print('Y shape : ',Y.shape)
+    families = ['gaussian']*X.shape[1]+['binomial']*Y.shape[1]
+    data_train_class = numpy.c_[X,Y]
+    spn = LearnSPN(cache=memory, row_cluster_method="RandomPartition",ind_test_method="subsample",alpha=alpha, min_features_slice=30, min_instances_slice=min_slices, cluster_prep_method=None, families=families).fit_structure(data_train_class)
+    return spn
+
+def spnClassificationGeneralPred(model, X, maxClasses):
+    print("Total number of instances : ",X.shape[0])
+    predictions = numpy.zeros(X.shape[0],maxClasses)
+    for i,feature in enumerate(X):
+        if(i%100 == 0):
+            print('Number of instances completed : ',i)
+        loglikelihood = numpy.zeros(maxClasses)
+        for j in range(maxClasses):
+            Y = numpy.zeros(maxClasses)
+            Y[j] = 1
+            data = numpy.transpose(numpy.r_[feature,Y])
+            ll = model.eval(data, individual=True)
+            loglikelihood[j] = ll[0]
+        predictions[i] = loglikelihood
+    return (predictions)
+
+
+if __name__ == '__main__':
+    in_train = numpy.loadtxt("data/food/train/_preLogits.csv", delimiter=",")
+    out_train = numpy.loadtxt("data/food/train/_groundtruth.csv", delimiter=",").astype(int)
+    
+    in_test = numpy.loadtxt("data/food/validation/_preLogits.csv", delimiter=",")
+    out_test = numpy.loadtxt("data/food/validation/_groundtruth.csv", delimiter=",").astype(int)
+    
+    
+    # Get indices of positive labels in ground truth
+    groundTruth_labels = [numpy.nonzero(i)[0] for i in out_test]
+        
+    nnpred_test = numpy.loadtxt("data/food/validation/_predictions.csv", delimiter=",")
+    
+    # sort logits descanding
+    # There is no direct method of getting argsort descending, so first 
+    # sort in increasing, then reverse the list 
+    np_sorted_logits_asc = numpy.argsort(numpy.loadtxt("data/food/validation/_logits.csv", delimiter=","), 1)
+    np_sorted_logits_desc = []
+    for _, e in enumerate(np_sorted_logits_asc):
+        np_sorted_logits_desc.append(e[::-1])
+
+    mAP_4 = mapk(groundTruth_labels, np_sorted_logits_desc, k=4)
+    print('mAP_4: %f' % mAP_4)
+    mAP_6 = mapk(groundTruth_labels, np_sorted_logits_desc, k=6)
+    print('mAP_6: %f' % mAP_6)
+    mAP_8 = mapk(groundTruth_labels, np_sorted_logits_desc, k=8)
+    print('mAP_8: %f' % mAP_8)
+    mAP_10 = mapk(groundTruth_labels, np_sorted_logits_desc, k=10)
+    print('mAP_10: %f' % mAP_10)
+    mAP_15 = mapk(groundTruth_labels, np_sorted_logits_desc, k=15)
+    print('mAP_15: %f' % mAP_15)
+    mAP_20 = mapk(groundTruth_labels, np_sorted_logits_desc, k=20)
+    print('mAP_20: %f' % mAP_20)
+    mAP_30 = mapk(groundTruth_labels, np_sorted_logits_desc, k=30)
+    print('mAP_30: %f' % mAP_30)
+    mAP_41 = mapk(groundTruth_labels, np_sorted_logits_desc, k=41)
+    print('mAP_41: %f' % mAP_41)
+
+    numClasses = out_train.shape[1]
+    start_time = time.time()
+    spnmodel = spnClassificationGeneralFit(in_train,out_train,numClasses,min_slices=100)
+    print("Learning SPN took %s seconds ---\n" % (time.time() - start_time))
+    print("Testing Deep SPN...\n")
+    start_time = time.time()
+    spnPred = spnClassificationGeneralPred(spnmodel,in_test,numClasses)
+    print("Testing SPN took %s seconds ---\n" % (time.time() - start_time))
+    spnPredLabels = getTopIndices(spnPred)
+    spnmAP_4 = mapk(groundTruth_labels, spnPredLabels, k=4)
+    print('mAP_4: %f' % spnmAP_4)
+    
